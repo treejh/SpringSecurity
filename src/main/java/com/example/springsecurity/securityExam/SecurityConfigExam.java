0f85e1e69ef2240dbb0de26cd7183e63a224b479
jihyun <jihyun3066@naver.com>
2025-02-27 15:45:23 +0900

    [ feat ] 원하는 url은 권한 없이 접근이 가능할 수 있게, 커스텀 가능

--- src/main/java/com/example/springsecurity/securityExam/SecurityConfigExam.java
+++ src/main/java/com/example/springsecurity/securityExam/SecurityConfigExam.java
@@ -18,7 +18,16 @@ public class SecurityConfigExam {
     public SecurityFilterChain securityFilterChain(HttpSecurity http )throws Exception{
 
         //anyRequest : 모든 요청에 대해서 인증을 요구하겠다.
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+//        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults());
+
+
+        //hello, loginForm은 권한 없이 접근이 가능하다.
+        http.authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/hello","/loginForm")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
                 .formLogin(Customizer.withDefaults());
 
         //return하려는 http .build붙이는거나, 위에 build를 빼고 http.build()나 똑같다 -> stream의 collect 같은거임 ㅇㅇ
